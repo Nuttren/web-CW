cbd3ff9d5759fbe4b704d2be667fbe18c77b85b7
nuttren <roman.karpencko@gmail.com>
2023-06-22 15:48:49 +0400

    Initial commit

---
+++ src/main/java/ru/skypro/lessons/springboot/webcw/WebCwApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.webcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/lessons/springboot/webcw/WebCwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.webcw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
